6dbb3229bf32a53d64d6da132cbe88f2f4adeab3
Doctorzik <ezekieljeremiah92@gmail>
2024-06-27 12:52:52 +0100

    feat: added the footer component to the layout

--- handcrafted/app/page.tsx
+++ handcrafted/app/page.tsx
@@ -6,8 +6,9 @@ import styles from '@/app/ui/home.module.css';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+
       <div className={`${styles.banner} flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52`}>
-        {/* <AcmeLogo /> */}
+        {/* <AcmeLogo /> */} 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -16,7 +17,7 @@ export default function Page() {
           </p>
           <p className={`${styles.text} md:text-2xl md:leading-normal`}>
             The go-to destination for those seeking unique, handcrafted treasures.
-          </p> 
+          </p>
           <Link
             href="/login"
             className={`${styles.banner} flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base`}
@@ -27,7 +28,9 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
+
       </div>
+
     </main>
   );
 }
