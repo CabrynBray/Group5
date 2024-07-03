5d862f3a7efd2cc63ef363dc2cb3dcef6b223119
Doctorzik <ezekieljeremiah92@gmail>
2024-07-03 17:24:36 +0100

    correct footer erro

--- handcrafted/app/ui/footer.tsx
+++ handcrafted/app/ui/footer.tsx
@@ -2,10 +2,9 @@
 import React from 'react';
 import styles from '../ui/home.module.css'
 import Link from 'next/link';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4fc442165ae956cef8f74c733269d2346930d984
+
+
+
 import AcmeLogo from './acme-logo';
 export default function Footer() {
     return (
@@ -13,15 +12,12 @@ export default function Footer() {
 
             <div className="container mx-auto px-4">
                 <AcmeLogo />
-<<<<<<< HEAD
-=======
-export default function Footer() {
-    return (
-        <footer className="bg-[#906951] text-white p-4 m-6 rounded-lg">
+            </div>
+
             <div className="container mx-auto px-4">
->>>>>>> 8ad0563 (feat: added the footer component to the layout)
-=======
->>>>>>> 4fc442165ae956cef8f74c733269d2346930d984
+
+
+
                 <p className="text-center">&copy; {new Date().getFullYear()} HANDCRAFTEDHAVEN. All rights reserved.</p>
                 <nav>
                     <ul className="flex justify-center space-x-4 mt-4">
