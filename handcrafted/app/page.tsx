8171f6cc8b706b12c083e4c7714e94e8c2e91f46
Christopher Cortes <christopherctes@hotmail.com>
2024-07-05 17:33:39 -0400

    header color

--- handcrafted/app/page.tsx
+++ handcrafted/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
 
-      <div className={`${styles.banner} flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52`}>
+      <div className={`${styles.header} flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52`}>
         {/* <AcmeLogo /> */} 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
