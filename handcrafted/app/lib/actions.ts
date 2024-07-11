2cdc93ab39b3ab52a050d1f77a0d793d3cad15d7
Doctorzik <ezekieljeremiah92@gmail>
2024-07-11 16:07:17 +0100

    remove customers photos

--- handcrafted/app/lib/actions.ts
+++ handcrafted/app/lib/actions.ts
@@ -1,11 +1,15 @@
 'use server';
-const { db } = require('@vercel/postgres');
+const { db, sql } = require('@vercel/postgres');
 import { z } from 'zod';
-import { sql } from '@vercel/postgres';
+
+const multer = require('multer');
+
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
+import { NextRequest } from 'next/server';
+import { request } from 'https';
 const bcrypt = require('bcrypt');
 export async function authenticate(
   prevState: string | undefined,
@@ -39,6 +43,20 @@ const userFormData = z.object({
   address: z.string(),
 });
 
+const poductsFormData = z.object({
+  user_id: z.string(),
+  name: z.string().toLowerCase(),
+  description: z.string(),
+  price: z.coerce.number(), // Corse the price to number because input filed alwasy return string
+  image_url: z.object({}),
+  /* user_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  price DECIMAL(10, 2) NOT NULL,
+  image_url VARCHAR(255),
+*/
+});
+const ProductForm = poductsFormData.omit({ image_url: true });
 export type State = {
   errors?: {
     fname?: string[];
@@ -50,9 +68,9 @@ export type State = {
   message?: string | null;
 };
 
- export async function userRegistration(formData: FormData) {
+export async function userRegistration(formData: FormData) {
   // Validate the UserformData with the actual form data passed into this function
-  const { fname, lname, email, password, address } =  userFormData.parse( {
+  const { fname, lname, email, password, address } = userFormData.parse({
     fname: formData.get('fname'),
     lname: formData.get('lname'),
     email: formData.get('email'),
@@ -60,20 +78,34 @@ export type State = {
     address: formData.get('address'),
   });
 
-
-// Hash the passoword before inserting it into the database.
+  // Hash the passoword before inserting it into the database.
   const hassPassword = await bcrypt.hash(password, 10);
 
   try {
-    
- const registerUser =   await sql`insert into Users(fname, lname, email, password, address) 
+    const registerUser =
+      await sql`insert into Users(fname, lname, email, password, address) 
           values(${fname}, ${lname}, ${email}, ${hassPassword}, ${address})`;
 
-   return registerUser
+    return registerUser;
   } catch (error) {
-      console.log("An error occured", error)
+    console.log('An error occured', error);
   }
 
   // revalidatePath('');
   redirect('/login');
 }
+
+// export default async function addProucts(formData: FormData) {
+//   console.log(formData );
+//   const storage = multer.diskStorage({
+//     destination: 'uploads',
+//   });
+//   multer({ storage: storage }).single('');
+
+//   // const { user_id, name, description, price } = ProductForm.parse({
+//   //   user_id: formData.get('user_id'),
+//   //   name: formData.get('name'),
+//   //   description: formData.get('description'),
+//   //   price: formData.get('price'),
+//   // });
+// }
