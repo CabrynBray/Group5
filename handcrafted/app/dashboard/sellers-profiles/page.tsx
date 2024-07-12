c7de8df8b8914c3a1b65fc0089d9555011e4bbe3
Christopher Cortes <christopherctes@hotmail.com>
2024-07-12 15:31:48 -0400

    sellers profile details

--- handcrafted/app/dashboard/sellers-profiles/page.tsx
+++ handcrafted/app/dashboard/sellers-profiles/page.tsx
@@ -27,14 +27,14 @@ export default async function Page({
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search for a seller..." />
-        <CreateInvoice />
+        {/* <CreateInvoice /> */}
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
-      <div className="mt-5 flex w-full justify-center">
+      {/* <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
-      </div>
+      </div> */}
     </div>
   );
 }
